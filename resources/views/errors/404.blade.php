99d0c24f162e188d53a1bf63133d18d31fa39b3f
Japs <ambivjaps@gmail.com>
2021-10-27 01:59:02 +0800

    Updated Error 404 Page

--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -19,19 +19,20 @@
 		<link rel="stylesheet" type="text/css" href="{{ URL::asset('/css/style.css') }}?<?php echo time(); ?>">
 		<link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous"/>
 		<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-
 	</head>
 
-
-
-	<div class="container">
-		<div class="row my-5">
-			<div class="col-md-9">
-				<h1> <img width="10%" src="{{ URL::asset('/img/ft_icon.png') }}"> Page not found. </h1>
-				<i> The page you are looking for does not exist, is changed, or currently not available. </i>
-				<a class="btn btn-warning mt-4" href="{{ URL::asset('/') }}" role="button">Go back to website</a>
-			</div>
+	<body>
+		<div class="container">
+			<center>
+				<img src="{{ URL::asset('/img/ft_icon.png') }}">
+				<h1 class="mt-5"> Error 404 </h1>
+				<hr>
+				<h4> Page not found. </h4>
+				<i> The page you are looking for does not exist, <br> is changed, or currently not available. </i>
+				<br><br>
+				<a class="btn btn-warning" href="{{ URL::asset('/') }}" role="button">Go back to website</a>
+			</center>
 		</div>
-	</div>
+	</body>
 
-@include('layouts.footer')
+</html>
