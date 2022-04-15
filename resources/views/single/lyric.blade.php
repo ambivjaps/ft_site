78081ed0d8afa8dd2814a8a15d52956f212dfbee
singjaps <singjaps@gmail.com>
2022-04-15 14:15:56 +0800

    Updated lyric page with jumbotron

--- resources/views/single/lyric.blade.php
+++ resources/views/single/lyric.blade.php
@@ -14,22 +14,25 @@
 
 @section('content')
 
-	<div class="container">
+	<div class="ft-event p-5 mb-3">
+		<div class="container-fluid py-2">
+			<div class="row">
+				<div class="col-md-2 mb-3">
+					<img class="rounded" src="{{ Voyager::image( $lyric->art ) }}" style="width:100%" title="{{ $lyric->title }}">
+				</div>
 
-		<div class="row my-5">
-			<div class="col-md-2 col-4">
-				<img src="{{ Voyager::image( $lyric->art ) }}" style="width:100%">
-			</div>
-
-			<div class="col-md-5 col-8">
-				<h4>{{ $lyric->title }}</h4>
-				<hr>
-				<h6> {{ $lyric->album }} </h6>
-				<h6> {{ $lyric->artist }} </h6>
-				@if($lyric->producer != 0) <h6> Producer: {{ $lyric->producer }} </h6> @endif
-				<div class="ft-year"><span class="badge bg-warning"> {{ $lyric->year }} </div></span>
+				<div class="col-md-9">
+					<h2 class="display-7 fw-bold"> {{ $lyric->title }} </h2><hr>
+					<h6> {{ $lyric->album }} </h6>
+					<h6> {{ $lyric->artist }} </h6>
+					@if($lyric->producer != 0) <h6> Producer: {{ $lyric->producer }} </h6> @endif
+					<div class="ft-year"><span class="badge bg-warning"> {{ $lyric->year }} </div></span>
+				</div>
 			</div>
 		</div>
+	</div>
+
+	<div class="container">
 
 		<div class="row my-5">
 			<div class="col-md-9 col-md-offset-2">			
